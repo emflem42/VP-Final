353b8abf5756a00da5509f30d01abbf01fc98b0a
kristenYu <yu155@mail.chapman.edu>
2014-12-02 21:10:53 -0800

    Obstacles

---
+++ Kristen/New Unity Project/Assets/DoorScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+	public GameObject myTrigger;
+	//public Trigger triggerScript;
+	// Use this for initialization
+	void Start () {
+	//	triggerScript = myTrigger.GetComponent<Trigger> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//openDoorLeft ();
+	
+	}
+	/*
+	public void openDoorLeft()
+	{
+		if (triggerScript.on == true) 
+		{
+			transform.Translate(Vector3.left * -1);
+		}
+	}*/
+}
