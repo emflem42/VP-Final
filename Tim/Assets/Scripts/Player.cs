8af262a44aedc2bd8ff350a6f7248d32cd70f48e
Tranklizer <trank101@mail.chapman.edu>
2014-12-08 08:50:32 -0800

    Player and Ghosts
    The project has scripts for controlling the player movement as well as
    managing the recordings. Right now, the scripts can:
    
    Record player movement to one of three arrays based on which recording
    button is clicked
    Overwrite existing recordings if the same record button is clicked twice
    Move Ghosts based on which recording they have been assigned to read
    Stop recording progress and reset the player and all Ghosts to their
    starting positions
    
    The Scripts CANNOT:
    Overwrite all recordings at once (there is no master "reset" button)
    Play back the recordings without the player moving

---
+++ Tim/Assets/Scripts/Player.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	int moveDistance = 1;
+	public RecordingInfo recorder; //RecordingInfo script
+	public GameObject recorderObject; //RecordingManager game object
+	public Vector3 targetPosition;
+	public string currentlySelectedArray;
+	public int movementCounter = 0;
+	public string[] currentArray;
+	public Vector3 recordingStartPosition;
+	public bool isRecording = false;
+	public float moveDelay = 1.0f;
+	public float moveTimer = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		recorder = recorderObject.GetComponent<RecordingInfo> ();
+		recordingStartPosition = transform.position;
+		targetPosition = transform.position; //Set the target position equal to the initial position
+		currentlySelectedArray = "recording 1";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentArray = setArray (currentlySelectedArray); //Decide which array in RecordingInfo to use
+
+		if (moveTimer <= 1.1f) 
+		{
+			moveTimer += Time.deltaTime; //Increase the timer up to 1 second
+		}
+
+		if (isRecording)
+		{
+			if (Input.GetKeyDown (KeyCode.LeftArrow) && moveTimer >= 1.0f) {
+				targetPosition = new Vector3 (transform.position.x - moveDistance, transform.position.y, transform.position.z);
+				AddMovement (currentArray, movementCounter, "Left");
+				movementCounter++;
+				moveTimer = 0.0f;
+			}
+
+			if (Input.GetKeyDown (KeyCode.RightArrow) && moveTimer >= 1.0f) {
+				targetPosition = new Vector3 (transform.position.x + moveDistance, transform.position.y, transform.position.z);
+				AddMovement (currentArray, movementCounter, "Right");
+				movementCounter++;
+				moveTimer = 0.0f;
+			}
+
+			if (Input.GetKeyDown (KeyCode.UpArrow) && moveTimer >= 1.0f) {
+				targetPosition = new Vector3 (transform.position.x, transform.position.y + moveDistance, transform.position.z);
+				AddMovement (currentArray, movementCounter, "Up");
+				movementCounter++;
+				moveTimer = 0.0f;
+			}
+
+			if (Input.GetKeyDown (KeyCode.DownArrow) && moveTimer >= 1.0f) {
+				targetPosition = new Vector3 (transform.position.x, transform.position.y - moveDistance, transform.position.z);
+				AddMovement (currentArray, movementCounter, "Down");
+				movementCounter++;
+				moveTimer = 0.0f;
+			}
+
+			transform.position = Vector3.Lerp (transform.position, targetPosition, 0.3f);
+		}
+
+	}
+
+	void AddMovement(string[] array, int index, string movement)
+	{
+		array [index] = movement;
+	}
+
+	public string[] setArray(string array)//Decides what array within RecordingInfo to use based on the string "currentlySelectedArray"
+	{
+		switch (array) 
+		{
+		case "recording 1":
+			return recorder.recording1;
+		case "recording 2":
+			return recorder.recording2;
+		case "recording 3":
+			return recorder.recording3;
+		default:
+			return null;
+		}
+	}
+}
---
+++ Tim/Assets/Scripts/RecordingTrigger.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class RecordingTrigger : MonoBehaviour {
+
+	public RecordingInfo recorder;
+	public GameObject recorderObject;
+	public GameObject playerObject;
+	public Player playerScript;
+	public string recordingNumber;
+	public Vector3 startPosition;
+
+	// Use this for initialization
+	void Start () {
+		playerScript = playerObject.GetComponent<Player> ();
+		recorder = recorderObject.GetComponent<RecordingInfo> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown()
+	{
+		if (playerScript.isRecording == false) 
+		{
+			playerScript.currentlySelectedArray = recordingNumber;
+			playerScript.isRecording = true;
+			playerObject.transform.position = startPosition;
+			playerScript.targetPosition = startPosition;
+			playerScript.recordingStartPosition = startPosition;
+
+			switch (recordingNumber) //Tells which array within RecordingInfo this trigger will be associated with
+			{
+			case "recording 1":
+				Array.Clear(recorder.recording1,0,1000); //Clears every movement within the RecordingInfo array associated with this trigger.
+				break;
+			case "recording 2":
+				Array.Clear(recorder.recording2,0,1000);
+				break;
+			case "recording 3":
+				Array.Clear(recorder.recording3,0,1000);
+				break;
+			default:
+				Debug.Log("No recordings found");
+				break;
+			}
+		}
+	}
+}
