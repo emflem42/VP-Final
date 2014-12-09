dbb46c5471dba8ed3f5dede685c91a118dfbeeb8
Erin Fleming <erin.m.fleming@gmail.com>
2014-12-08 23:01:18 -0800

    Player Animation
    Right/left walking and idle animation finished. Working on verticals
    now.

---
+++ Erin/CharacterAnim/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 6.0f;
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Movement();
+	}
+
+	void Movement() //function that stores all the movement
+	{
+		anim.SetFloat("speed", Mathf.Abs(Input.GetAxis ("Horizontal")));
+		
+		if(Input.GetAxisRaw("Horizontal") > 0)
+		{
+			transform.Translate(Vector3.right * speed * Time.deltaTime); 
+			transform.eulerAngles = new Vector2(0, 0); //this sets the rotation of the gameobject
+		}
+		
+		if(Input.GetAxisRaw("Horizontal") < 0)
+		{
+			transform.Translate(Vector3.right * speed * Time.deltaTime);
+			transform.eulerAngles = new Vector2(0, 180); //this sets the rotation of the gameobject
+		}
+	}
+}
